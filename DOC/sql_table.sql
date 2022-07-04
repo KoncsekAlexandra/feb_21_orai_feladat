6cd722489f8f9e5a560d84174a15e7bc7917662f
KoncsekAlexandra <96825964+KoncsekAlexandra@users.noreply.github.com>
2022-07-04 09:53:55 +0200

    Add files via upload

---
+++ DOC/sql_table.sql
@@ -0,0 +1,9 @@
+-- CREATE DATABASE tanverseny;
+-- USE tanverseny;
+
+CREATE TABLE tanverseny (
+  azonosito INT PRIMARY KEY, 
+  versenynev TEXT, 
+  tipus VARCHAR(100), 
+  esemeny VARCHAR(100), 
+  datum DATE);
